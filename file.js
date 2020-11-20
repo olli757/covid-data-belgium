2877ed7d13c8852cb78b5be0ece6ec48ddf84244
Olivier Moens <olivier.moens.1@student.kdg.be>
2020-11-20 23:37:24 +0100

    added chart for hospital IN and OUT

--- file.js
+++ file.js
@@ -4,21 +4,76 @@ Credits to :
 The Coding Train for making a great tutorial on how to build charts in JS with Charts.js
 https://www.youtube.com/watch?v=5-ptp9tRApM
 
-Source of the data is
+Sources of the data are:
+
 European Centre for Disease Prevention and Control
 https://www.ecdc.europa.eu/en/publications-data/download-todays-data-geographic-distribution-covid-19-cases-worldwide
 Downloaded 12 November 2020
+
+Sciensano
+https://epistat.wiv-isp.be/covid/
+
+NEW_IN
+Number of new lab-confirmed hospital intakes in the last 24h (incidence) not
+referred to another hospital
+
+NEW_OUT
+Number of new lab-confirmed hospital discharges (alive) in the last 24h
+(incidence) not referred to another hospital
+
+Change in definition on 28/04/2020:
+Given the enlarged test indications since 22/04/2020 (all patients admitted to the
+hospital can be tested, regardless of the reason for admission) and given the
+gradual return to normal hospital activities, the new lab-confirmed admissions are
+now subdivided according to pathology, i.e. COVID-19 lab-confirmed patients
+admitted to the hospital because of COVID-19, and COVID-19 lab-confirmed
+patients admitted to the hospital because of another pathology but who tested
+positive within a screening context. The latter are not included in the variable
+NEW_IN.
+
 
 */
 
 window.addEventListener('load', setup);
 
 async function setup() {
+    const ctxHosp = document.getElementById('hospChart').getContext('2d');
     const ctx = document.getElementById('casesChart').getContext('2d');
     const ctx2 = document.getElementById('deathsChart').getContext('2d');
     const data = await getData();
-    console.log(data);
-    const chart = new Chart(ctx, {
+    const hospData = await getHosp();
+    console.log(hospData);
+
+    const hospChart = new Chart(ctxHosp, {
+      // The type of chart we want to create
+      type: 'bar',
+
+      // The data for our dataset
+      data: {
+          labels: hospData.dates,
+          datasets: [{
+              label: 'Number of new lab-confirmed hospital intakes in the last 24h',
+              backgroundColor: 'green',
+              borderColor: 'rgb(255, 99, 132)',
+              data: hospData.newIns
+          }, {
+              label: 'Number of new lab-confirmed hospital discharges (alive) in the last 24h',
+              backgroundColor: 'red',
+              borderColor: 'rgb(255, 99, 132)',
+              data: hospData.newOuts
+          }]
+      },
+
+      // Configuration options go here
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        responsiveAnimationDuration: 0
+        
+      }
+  });
+
+    const casesChart = new Chart(ctx, {
         // The type of chart we want to create
         type: 'bar',
 
@@ -46,7 +101,7 @@ async function setup() {
         }
     });
 
-    const chart2 = new Chart(ctx2, {
+    const deathsChart = new Chart(ctx2, {
       // The type of chart we want to create
       type: 'bar',
 
@@ -88,4 +143,40 @@ async function getData() {
     deaths.unshift(deathcount);
   });
   return { dates, cases, deaths};
+}
+
+async function getHosp() {
+  const response = await fetch('COVID19BE.csv');
+  const data = await response.text();
+  const rows = data.split("\n").slice(1);
+  dates = [];
+  newIns = [];
+  newOuts = [];
+  let previousDate = '2020-03-15';
+  let newInTotal = 0;
+  let newOutTotal = 0;
+  rows.forEach(row => {
+    const cols = row.split(",");
+    const date = cols[0];
+    const newInCurrentLine = parseInt(cols[8]);
+    const newOutCurrentLine = parseInt(cols[9]);
+
+    if (previousDate === date) {
+      dates.pop();
+      newIns.pop();
+      newOuts.pop();
+      newInTotal += newInCurrentLine;
+      newOutTotal += newOutCurrentLine;
+    } else {
+      newInTotal = newInCurrentLine;
+      newOutTotal = newOutCurrentLine;
+      previousDate = date;
+    }
+
+    dates.push(date);
+    newIns.push(newInTotal); 
+    newOuts.push(newOutTotal);
+  });
+
+  return { dates, newIns, newOuts };
 }
